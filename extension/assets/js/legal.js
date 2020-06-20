c3c87c91ac0e76f97ff4ca76a39fa9c5713e7f9f
sebastian-meier <contact@sebastianmeier.eu>
2020-06-20 18:21:11 +0200

    legal

--- extension/assets/js/legal.js
+++ extension/assets/js/legal.js
@@ -29529,11 +29529,39 @@ __webpack_require__.r(__webpack_exports__);
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _nav__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./nav */ "./src/typescript/nav.ts");
 
-var translateInstall = [
-    "howToTitle",
+var legalTranslate = [
+    ["legal--title", "legalTitle"],
+    ["legal--headline1", "legalHeadline1"],
+    ["legal--copy1", "legalCopy1"],
+    ["legal--headline2", "legalHeadline2"],
+    ["legal--copy2", "legalCopy2"],
+    ["legal--headline3", "legalHeadline3"],
+    ["legal--copy3", "legalCopy3"],
+    ["legal--headline4", "legalHeadline4"],
+    ["legal--copy4", "legalCopy4"],
+    ["legal--copy5", "legalCopy5"],
+    ["legal--copy6", "legalCopy6"],
+    ["legal--copy7", "legalCopy7"],
+    ["legal--copy8", "legalCopy8"],
+    ["legal--copy9", "legalCopy9"],
+    ["legal--headline5", "legalHeadline5"],
+    ["legal--copy10", "legalCopy10"],
+    ["legal--copy11", "legalCopy11"],
+    ["legal--copy12", "legalCopy12"],
+    ["legal--copy13", "legalCopy13"],
+    ["legal--copy14", "legalCopy14"],
+    ["legal--copy15", "legalCopy15"],
+    ["legal--headline6", "legaleadline6"],
+    ["legal--copy16", "legalCopy16"],
+    ["legal--copy17", "legalCopy17"],
+    ["legal--headline7", "legalHeadline7"],
+    ["legal--copy18", "legalCopy18"],
+    ["legal--copy19", "legalCopy19"],
+    ["legal--headline8", "legalHeadline8"],
+    ["legal--copy20", "legalCopy20"],
 ];
-translateInstall.forEach(function (t) {
-    // document.querySelector("#" + t).innerHTML = browser.i18n.getMessage(t);
+legalTranslate.forEach(function (t) {
+    document.querySelector("#" + t[0]).innerHTML = browser.i18n.getMessage(t[1]);
 });
 Object(_nav__WEBPACK_IMPORTED_MODULE_0__["setupVersion"])();
 Object(_nav__WEBPACK_IMPORTED_MODULE_0__["setupFooter"])();
--- src/typescript/legal.ts
+++ src/typescript/legal.ts
@@ -1,11 +1,39 @@
 import { setupVersion, setupFooter } from "./nav";
 
-const translateInstall = [
-  "howToTitle",
+const legalTranslate = [
+  ["legal--title", "legalTitle"],
+  ["legal--headline1", "legalHeadline1"],
+  ["legal--copy1", "legalCopy1"],
+  ["legal--headline2", "legalHeadline2"],
+  ["legal--copy2", "legalCopy2"],
+  ["legal--headline3", "legalHeadline3"],
+  ["legal--copy3", "legalCopy3"],
+  ["legal--headline4", "legalHeadline4"],
+  ["legal--copy4", "legalCopy4"],
+  ["legal--copy5", "legalCopy5"],
+  ["legal--copy6", "legalCopy6"],
+  ["legal--copy7", "legalCopy7"],
+  ["legal--copy8", "legalCopy8"],
+  ["legal--copy9", "legalCopy9"],
+  ["legal--headline5", "legalHeadline5"],
+  ["legal--copy10", "legalCopy10"],
+  ["legal--copy11", "legalCopy11"],
+  ["legal--copy12", "legalCopy12"],
+  ["legal--copy13", "legalCopy13"],
+  ["legal--copy14", "legalCopy14"],
+  ["legal--copy15", "legalCopy15"],
+  ["legal--headline6", "legaleadline6"],
+  ["legal--copy16", "legalCopy16"],
+  ["legal--copy17", "legalCopy17"],
+  ["legal--headline7", "legalHeadline7"],
+  ["legal--copy18", "legalCopy18"],
+  ["legal--copy19", "legalCopy19"],
+  ["legal--headline8", "legalHeadline8"],
+  ["legal--copy20", "legalCopy20"],
 ];
 
-translateInstall.forEach((t) => {
-  // document.querySelector("#" + t).innerHTML = browser.i18n.getMessage(t);
+legalTranslate.forEach((t) => {
+  document.querySelector("#" + t[0]).innerHTML = browser.i18n.getMessage(t[1]);
 });
 
 setupVersion();
