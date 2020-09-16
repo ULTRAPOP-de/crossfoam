552667c28bc02167fe4b9826f565db288f0f11ce
Sebastian Meier <contact@sebastianmeier.eu>
2020-09-16 14:54:53 +0200

    opera cleaning job

--- src/typescript/options-light.ts
+++
@@ -1,9 +0,0 @@
-document.getElementById("clicker").addEventListener("click", () => {
-  browser.tabs.create({
-    url: "/html/options.html",
-  }).then((results) => {
-    // successful opening options.html
-  }, (err) => {
-    throw err;
-  });
-});
